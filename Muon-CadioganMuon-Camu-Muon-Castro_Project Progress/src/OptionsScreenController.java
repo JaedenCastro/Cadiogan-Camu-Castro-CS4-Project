cd8041c0ce213180416f6002eb3ea54caaadf8ab
Kurisitofu1205 <72328312+Kurisitofu1205@users.noreply.github.com>
2023-05-02 08:52:35 +0800

    Add files via upload

--- Muon-CadioganMuon-Camu-Muon-Castro_Project Progress/src/OptionsScreenController.java
+++ Muon-CadioganMuon-Camu-Muon-Castro_Project Progress/src/OptionsScreenController.java
@@ -6,12 +6,16 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.Media;
 
 /**
  *
  * @author windows10 user
  */
 public class OptionsScreenController extends ControllerBase implements Initializable {
+    
     
     @FXML
     private Label label;
@@ -20,10 +24,21 @@ public class OptionsScreenController extends ControllerBase implements Initializ
     private void back (ActionEvent event) throws IOException {
         newScreen(event, "PauseScreen.fxml");
     }
-
+    
+    private MediaPlayer mediaPlayer;
+    private Slider volumeSlider;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        Media media = new Media("file:///path/to/mediafile.mp3");
+        mediaPlayer = new MediaPlayer(media);
+        
+         volumeSlider = new Slider(0, 1, 1);
+        volumeSlider.setOrientation(Orientation.HORIZONTAL);
+        volumeSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            mediaPlayer.setVolume(newValue.doubleValue());
+        });
     }    
     
 }
